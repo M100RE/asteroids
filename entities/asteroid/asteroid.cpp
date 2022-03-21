b26bbb08560f7fd3e5adc8ffb2ef1c158e4a8266
M100RE <maximilian@host.localdomain>
2022-03-21 21:35:00 +0100

    Committer: M100RE <maximilian@host.localdomain>
     On branch asteroids
     Changes to be committed:
    	modified:   asteroid.cpp
    	modified:   asteroid.h
    
        Made constructor simpler, so vertices, and data etc. doesnt have to
        be defined

--- entities/asteroid/asteroid.cpp
+++ entities/asteroid/asteroid.cpp
@@ -1,7 +1,17 @@
 #include "asteroid.h"
 
-asteroid::asteroid(float speed_x, float speed_y, float* vertices, int vertices_size, unsigned int* indices, int indices_size, const char* vertex_path, const char* fragment_path)
-    : entity(vertices, vertices_size, indices, indices_size, vertex_path, fragment_path)
+float vertices[] = 
+{
+
+};
+
+unsigned int indices[] = 
+{
+
+};
+
+asteroid::asteroid(float speed_x, float speed_y)
+    : entity(vertices, sizeof(vertices), indices, sizeof(indices), "shaders/vertex.shader", "shaders/fragment.shader")
 {
     define_speed_x(speed_x);
     define_speed_y(speed_y);
@@ -9,7 +19,7 @@ asteroid::asteroid(float speed_x, float speed_y, float* vertices, int vertices_s
 
 void asteroid::split()
 {
-
+    
 }
 
 void asteroid::update(GLFWwindow* window, int window_width, int window_height)
--- entities/asteroid/asteroid.h
+++ entities/asteroid/asteroid.h
@@ -6,7 +6,7 @@ class asteroid : entity
 {
 private:
 public: 
-    asteroid(float speed_x, float speed_y, float* vertices, int vertices_size, unsigned int* indices, int indices_size, const char* vertex_path, const char* fragment_path);
+    asteroid(float speed_x, float speed_y);
 
     void split();
     void update(GLFWwindow* window, int window_width, int window_height) override;
